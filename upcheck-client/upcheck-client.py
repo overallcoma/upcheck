1a0b59b47a0e5253e4cea84e9e83405d58d6e504
OAC <overallcoma@gmail.com>
2018-07-13 23:38:19 -0500

    Modem Status Page During Outage Added

--- upcheck-client/upcheck-client.py
+++ upcheck-client/upcheck-client.py
@@ -1,158 +1,62 @@
 import os
-import speedtest
 import datetime
-import schedule
-import sqlite3
+import requests
 import time
-from sqlite3 import Error
-from subprocess import call
-from xml.etree import ElementTree
-from xml.etree.ElementTree import Element
-from xml.etree.ElementTree import SubElement
-
-xml_web_output = "/usr/share/nginx/html/upcheck-speedtest.xml"
+import subprocess
 
 try:
-    dbfile = os.environ['UPCHECK_DB_LOCATION']
-except Error as e:
+    urltocheck = os.environ['UPCHECK_URLTOCHECK']
+except os.error as e:
     print(e)
     exit(1)
-# try:
-#     modem_log_page = os.environ['MODEM_LOG_PAGE']
-# except Error as e:
-#     print(e)
-#     exit(1)
 
 
-def dbconnect(dbfile):
+def create_nginx_index_page():
+    index_page = "/usr/share/nginx/html/index.html"
+    index_page_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>UpCheck-Client is Active</title>
+    </body>
+    </html>"""
+    create_index_page = open(index_page, "w+")
+    create_index_page.write(index_page_content)
+
+
+def check_if_up(url):
     try:
-        connection = sqlite3.connect(dbfile)
-        print(sqlite3.version)
-        connection.close()
-    except Error as t:
-        print(t)
-        exit(1)
-
-def db_createtable(dbfile):
-    try:
-        connection = sqlite3.connect(dbfile)
-        cursor = connection.cursor()
-        sql = 'CREATE TABLE IF NOT EXISTS upcheck (record_number integer PRIMARY KEY AUTOINCREMENT, timestamp TIMESTAMP, download INTEGER, upload INTEGER, ping INTEGER)'
-        cursor.execute(sql)
-    except Error as t:
-        print(t)
-        exit(1)
-
-def write_out_record(dbfile, timestamp, download, upload, ping):
-    try:
-        connection = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO upcheck VALUES (NULL, ?, ?, ?, ?)", (timestamp, download, upload, ping))
-        connection.commit()
-        connection.close()
-    except Error as t:
-        print(t)
-
-
-def average(input):
-    return sum(input) / len(input)
-
-def get_average_data():
-    connection = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
-    cursor = connection.cursor()
-    yesterday = (datetime.datetime.now()) - (datetime.timedelta(days=1))
-    cursor.execute("SELECT download FROM upcheck WHERE timestamp > ?", (yesterday,))
-    results = cursor.fetchall()
-    selected_results = []
-    for result in results:
-        selected_results.append(result[0])
-    speedtest_dl_averrage = average(selected_results)
-    speedtest_dl_averrage = round(speedtest_dl_averrage,2)
-    cursor.execute("SELECT upload FROM upcheck WHERE timestamp > ?", (yesterday,))
-    results = cursor.fetchall()
-    selected_results = []
-    for result in results:
-        selected_results.append(result[0])
-    speedtest_ul_average = average(selected_results)
-    speedtest_ul_average = round(speedtest_ul_average,2)
-    cursor.execute("SELECT ping FROM upcheck WHERE timestamp > ?", (yesterday,))
-    results = cursor.fetchall()
-    selected_results = []
-    for result in results:
-        selected_results.append(result[0])
-    speedtest_ping_average = average(selected_results)
-    speedtest_ping_average = round(speedtest_ping_average,2)
-    average_array = []
-    average_array.append(speedtest_dl_averrage)
-    average_array.append(speedtest_ul_average)
-    average_array.append(speedtest_ping_average)
-    return average_array
-
-
-def run_speedtest():
-    stest = speedtest.Speedtest()
-    stest.get_servers()
-    stest.get_best_server()
-    stest.download()
-    stest.upload()
-    return stest.results
-
-
-def bits_to_bytes(inputvalue):
-    bits = inputvalue
-    megabits = bits / 1000000
-    megabits = format(megabits, '.2f')
-    return megabits
-
-
-
-
-def primary_operation():
-    stest_result = run_speedtest()
-    download_speed = str(bits_to_bytes(stest_result.download))
-    upload_speed = str(bits_to_bytes(stest_result.upload))
-    ping = str(stest_result.ping)
-
-    timestamp = datetime.datetime.now()
-    write_out_record(dbfile, timestamp, download_speed, upload_speed, ping)
-
-    average_dl = str(get_average_data()[0])
-    average_ul = str(get_average_data()[1])
-    average_ping = str(get_average_data()[2])
-
-    xml_root = Element('SpeedtestResults')
-    SubElement(xml_root, "average_dl").text = average_dl
-    SubElement(xml_root, "average_ul").text = average_ul
-    SubElement(xml_root, "average_ping").text = average_ping
-
-    print(xml_root)
-    xml_output = ElementTree.tostring(xml_root, encoding='unicode')
-    xml_output_file = open(xml_web_output, "w+")
-    xml_output_file.write(xml_output)
+        check_return = requests.get(url)
+        check_return = check_return.status_code
+        if check_return == 200:
+            return 0
+        else:
+            currentime = datetime.datetime.now()
+            print("{0} -- Outage Detected".format(currentime))
+            return 1
+    except requests.exceptions.RequestException as e:
+        currentime = datetime.datetime.now()
+        print("{0} -- Outage Detected".format(currentime))
+        return 1
 
 
 try:
-    call(["nginx"])
-except Error as e:
+    subprocess.call(["nginx"])
+except subprocess.CalledProcessError as e:
     print(e)
     print("Unable to start nginx")
 
-try:
-    dbconnect(dbfile)
-except Error as e:
-    print("Could not connect to database")
-    print(e)
-    exit(1)
-try:
-    db_createtable(dbfile)
-    print("Upcheck is active")
-except Error as e:
-    print("Unable to Create Table")
-    print(e)
-    exit(1)
+outage_active = 0
+child_process = "./upcheck-client-scheduledtasks.py"
 
-primary_operation()
 
 while True:
-    schedule.every().hour.do(primary_operation)
-    time.sleep(1)
+    check_status = check_if_up(urltocheck)
+    if check_status == 0:
+        if outage_active == 1:
+            outage_active = 0
+    elif check_status == 1:
+        if outage_active == 0:
+            subprocess.Popen(['python3', child_process])
+            outage_active = 1
+    time.sleep(10)
--- upcheck-client/upcheck-modem-scrape.py
+++ upcheck-client/upcheck-modem-scrape.py
@@ -1,13 +1,15 @@
 import requests
+import html5lib
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
 modem_log_page = "http://192.168.100.1/cgi-bin/status_cgi"
+modem_web_output = "/usr/share/nginx/html/upcheck-modem.txt"
 
 
 def get_modem_log(table_number):
     modem_log_collect = requests.get(modem_log_page)
-    soup = BeautifulSoup(modem_log_collect.text, "html.parser")
+    soup = BeautifulSoup(modem_log_collect.text, "html5lib")
     table = soup.find_all('table')[table_number]
     table_body = table.find('tbody')
     rows = table_body.find_all('tr')
@@ -22,7 +24,16 @@ def get_modem_log(table_number):
             pretty_table_out.add_row(cols)
         pt_counter = pt_counter + 1
     print(pretty_table_out)
+    return pretty_table_out
 
 
-get_modem_log(1)
-# get_modem_log(3, "out2.csv") # This will work when they add closing tags to the damned Upstream Table
+log1 = str(get_modem_log(1))
+log2 = str(get_modem_log(3))
+log3 = str(get_modem_log(7))
+
+write_modem_output = open(modem_web_output, "w+")
+write_modem_output.write(log1)
+write_modem_output.write(log2)
+write_modem_output.write(log3)
+
+
